17e8a820722c0fa10559294f62e23698f87759dd
pirouz sourati <pirouz.sourati@outlook.com>
2024-06-20 12:15:46 +0000

    commit cdk changes

---
+++ S3/iac/cdk/lib/cdk-stack.ts
@@ -0,0 +1,18 @@
+import { Duration, Stack, StackProps } from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import { Construct } from 'constructs';
+import { RemovalPolicy } from 'aws-cdk-lib';
+
+export class CdkStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    new s3.Bucket(this, 'cdk-Bucket', {  // Changed scope to this
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      enforceSSL: true,
+      versioned: false,
+      removalPolicy: RemovalPolicy.RETAIN,
+    });
+  }
+}
